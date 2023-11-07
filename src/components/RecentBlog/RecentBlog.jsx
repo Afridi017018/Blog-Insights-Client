9712981cfd2c8a3ea73eca1f2aa2cc658503160c
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-11-07 23:27:32 +0600

    made recentBlogs dynamic

--- src/components/RecentBlog/RecentBlog.jsx
+++ src/components/RecentBlog/RecentBlog.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import useAuth from '../../hooks/useAuth';
+import useAxios from '../../hooks/useAxios';
 
-const RecentBlog = () => {
+const RecentBlog = ({element}) => {
+    const navigate = useNavigate();
+const axios = useAxios();
+const {user} = useAuth();
+
+    const handleAddToWishlist = async () => {
+        const res = await axios.post(`/add-to-wishlist`, { blogId: element._id, user: user?.email })
+        toast.dismiss();
+        toast.success(res.data.message);
+    }
+
     return (
         <div className='px-5 lg:px-0'>
             <div className="card card-side min-h-72 w-full shadow-xl border rounded-none">
                 <img className='min-h-full w-2/4 ' src="https://img.freepik.com/free-photo/one-person-standing-cliff-achieving-success-generated-by-ai_188544-11834.jpg?t=st=1699090908~exp=1699094508~hmac=da0835f35aee05e49cd9a814ab4bfbd33bc11ba83c5e4648a8d29b4bbb0d697a&w=826" alt="Album" />
                 <div className="card-body text-gray-500 capitalize">
-                <p className='text-gray-600 underline underline-offset-2'>Tour</p>
-                    <h2 className="card-title text-green-700">Travel On Sajek an hiamlos</h2>
+                <p className='text-gray-600 underline underline-offset-2 capitalize'>{element.category}</p>
+                    <h2 className="card-title text-green-700">{element.title}</h2>
                     
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, iure.</p>
+                    <p>{element.shortDesc}</p>
                     <div className="card-actions justify-center">
-                        <button onClick={()=>navigate(`/details`)} className="bg-gray-600 hover:bg-gray-800 px-2 py-1 w-28 rounded text-white text-sm">See Details</button>
-                        <button onClick={()=>navigate(`/updateProduct/`)} className="bg-green-800 hover:bg-green-950 px-2 py-1 w-28 rounded text-white text-sm">Add To Wishlist</button>
+                        <button onClick={()=>navigate(`/blog-details/${element._id}`)} className="bg-gray-600 hover:bg-gray-800 px-2 py-1 w-28 rounded text-white text-sm">See Details</button>
+                        <button onClick={handleAddToWishlist} className="bg-green-800 hover:bg-green-950 px-2 py-1 w-28 rounded text-white text-sm">Add To Wishlist</button>
                     </div>
                 </div>
             </div>
--- src/components/RecentBlogs/RecentBlogs.jsx
+++ src/components/RecentBlogs/RecentBlogs.jsx
@@ -1,18 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxios from '../../hooks/useAxios';
+import Loading from '../../pages/Loading/Loading';
 import RecentBlog from '../RecentBlog/RecentBlog';
 
 
+
 const RecentBlogs = () => {
+const axios = useAxios();
+    const getRecent = async () => {
+        const res = await axios.get(`/get-recent-blogs`);
+        return res;
+    }
+    
+    const { data, isLoading } = useQuery({
+        queryKey: ["recent"],
+        queryFn: getRecent,
+    })
+    
+    
+    if (isLoading) {
+        return <Loading />
+    }
+ 
+
+
+
     return (
         <div>
             <h2 className='text-2xl font-bold text-center text-gray-500 my-10 underline underline-offset-8'>Recent Blogs</h2>
             <div className='grid grid-cols-1 lg:grid-cols-2 container my-5 mx-auto gap-5'>
-                <RecentBlog />
-                <RecentBlog />
-                <RecentBlog />
-                <RecentBlog />
-                <RecentBlog />
-                <RecentBlog />
+                {/* <RecentBlog /> */}
+               
+               {
+                data.data.result.map((element)=>(
+                    <RecentBlog key={element._id} element={element} />
+                ))
+               }
                 
                 
 
